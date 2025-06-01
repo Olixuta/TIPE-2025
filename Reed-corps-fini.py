827990fff7f12cf9af63b99abfd0bbbffdddf22d
Olixuta <dorianpadovan@gmail.com>
2025-06-01 21:07:14 +0200

    Add files via upload

--- Reed-corps-fini.py
+++ Reed-corps-fini.py
@@ -6,6 +6,57 @@ import struct
 import numpy as np
 from random import random
 
+
+def repr_bin_str(c:str):
+    """converti un charactère en sa représentation binaire
+
+    Args:
+        c (str): un str de taille 1 
+
+    Returns:
+        str: représentation binaire de c 
+    """
+    return format((ord(c)),'07b')
+
+
+def codage_binaire(chaine:str):
+    """converti une chaine en une représentation bianire 
+
+    Args:
+        chaine (str): 
+
+    Returns:
+        str: concaténation de la représentation binaire de chacun des caractère de la chaine en entrée 
+    """
+    c=''
+    for elt in chaine:
+        c=c + repr_bin_str(elt)
+    return c
+
+
+
+
+def decoupage_taille_p(message:str,p:int):# yes
+    n = len(message)
+    q= n//p
+    r = n%p
+    l = ['' for i in range(p+1)]
+    for i in range(p+1):
+        c=''
+        for j in range(q):
+            if i*q + j < n : 
+                c=c+ message[i*q + j]
+            #print(i*q+j)
+        l[i]=c
+    if r != 0 :
+        #print((p)*q + q)
+        l.append(message[(p)*q+q:])# le dernier est 
+    return l 
+
+m= 'This is a simple ASCII text '
+n = len(m)
+print(decoupage_taille_p(m,n))
+
 #inverse = pow(a, -1, 131)
 
 def evaluation_li(i,x_values,eval,modulo=131):
@@ -66,3 +117,53 @@ def lagrange_poly(x, y, modulo):
         final_poly = poly_add(final_poly, li_scaled, modulo)
 
     return final_poly
+
+
+def format_polynomial(coeffs, modulo):
+    terms = []
+    for i, coeff in enumerate(coeffs):
+        coeff = coeff % modulo
+        if coeff == 0:
+            continue
+        if i == 0:
+            term = f"{coeff}"
+        elif i == 1:
+            term = f"{coeff}x" if coeff != 1 else "x"
+        else:
+            term = f"{coeff}x^{i}" if coeff != 1 else f"x^{i}"
+        terms.append(term)
+    
+    if not terms:
+        return "0"
+    return " + ".join(terms[::-1]) + f"  (mod {modulo})"
+
+
+x= [1,2,3,4]
+y =[3,1,5,0]
+print(lagrange(x,y,5,7))
+p = lagrange_poly(x,y,7)
+print(format_polynomial(p,7))
+
+x= [2,3,4,5]
+y =[1,5,0,6]
+print(lagrange(x,y,1,7))
+p=lagrange_poly(x,y,7)
+print(format_polynomial(p,7))
+
+
+# a modif 
+def encodage_reed_solomon(message:str,redondance:int,p:int):
+    n = len(message)
+    l = decoupage_taille_p(message,n)
+    ll=[ 0 for i in range(m+redondance)]
+    m = len(m)
+    x_points=[i for i in range(m)]
+    y_points=[ord(message[i]) for i in range(n)]
+
+    P=lagrange_poly(x_points,y_points,131)
+    for r in range(redondance):
+        #print(P(m+r))
+        #assert(abs(P(m+r))<=2**15)
+        l.append(P(m+r))
+
+    return l 
